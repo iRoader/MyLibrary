34a38b93611a3e9d04dcb4ce60aac8c32fd12e94
iRoader <iRoader@163.com>
2015-08-02 15:21:55 +0800

    MyLibrary
    创建一个我自己的js库。通过创建库把基础的JS封装起来，为以后调用。

---
+++ js/base.js
@@ -0,0 +1,28 @@
+﻿//对象式写法
+// var Base = {
+// 	getId:function(id){
+// 		return document.getElementById(id);
+// 	},
+// 	getName: function(name){
+// 		return document.getElementsByName(name);
+// 	},
+// 	getTagName: function(tag){
+// 		return document.getElementsByTagName(tag);
+// 	}
+// }
+
+//函数式写法
+function $(id){
+	return document.getElementById(id);
+}
+
+// 使用函数式写法
+// function Main(){
+// 	this.elements = [];
+
+// 	this.getId: function(id){
+// 		this.elements.push(document.getElementById(id));
+// 		return this;
+// 	}
+// }
+
---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	
+	// alert(Base.getId("div1").innerHTML);
+
+	alert($("div1").innerHTML);
+
+};
+
+
